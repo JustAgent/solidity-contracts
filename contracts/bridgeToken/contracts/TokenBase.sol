442021119271ca48a6441de3a11cb2d0d3dc674c
JustAgent <baykov.ivan.03@gmail.com>
2022-10-02 20:30:45 +0300

    token done

---
+++ contracts/bridgeToken/contracts/TokenBase.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: None
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Token is ERC20, Ownable {
+    constructor (string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+
+    function mint(address _account, uint256 _amount) external onlyOwner {
+        _mint(_account, _amount);
+    }
+    function burn(address _account, uint256 _amount) external onlyOwner {
+        _burn(_account, _amount);
+    }
+    
+}
