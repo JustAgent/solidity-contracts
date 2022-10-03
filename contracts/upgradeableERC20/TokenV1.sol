52afffa5fca79ab9de71f49632a00ef22016611a
JustAgent <baykov.ivan.03@gmail.com>
2022-10-03 09:19:05 +0300

    v1

---
+++ contracts/upgradeableERC20/TokenV1.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract Token is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+
+    function initialize() external initializer {
+        __ERC20_init("MyTokenV1", "MTV1");
+        __Ownable_init();
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+}
