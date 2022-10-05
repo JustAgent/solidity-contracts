068ce140c0a31fa308e6687122336a2bf4699831
JustAgent <baykov.ivan.03@gmail.com>
2022-10-05 09:31:59 +0300

    libraries

--- contracts/marketplaceNft/NftMarketplace.sol
+++ contracts/marketplaceNft/NftMarketplace.sol
@@ -2,12 +2,19 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Marketplace {
+contract Marketplace is Ownable, ReentrancyGuard {
     
+    startSell(address _contract, uint256 tokenId) public {
+        IERC721 nft = IERC721(_contractAddress);
+        require(nft.ownerOf(tokenId) == msg.sender, "You are not the owner");
+    }
+
+
 
     function _transferNFT(address _to, address, uint256 tokenId, address _contractAddress ) private {
         IERC721 nft = IERC721(_contractAddress);
@@ -20,3 +27,80 @@ contract Marketplace {
     }
 
 }
+
+contract TokenRecipient{
+    event ReceivedEther(address indexed sender, uint amount);
+    event ReceivedTokens(address indexed from, uint256 value, address indexed token, bytes extraData);
+
+    function receiveApproval(address from, uint256 value, address token, bytes calldata extraData) public returns(bool) {
+        IERC20 t = IERC20(token);
+        require(t.transferFrom(from, address(this), value));
+        emit ReceivedTokens(from, value, token, extraData);
+        return true;
+    }
+
+     function receiveETHApproval(address from, uint256 value) public  {
+        emit ReceivedEther(from, value);
+    }
+
+   
+    receive() payable external {
+        emit ReceivedEther(msg.sender, msg.value);
+        receiveETHApproval(msg.sender, msg.value);
+    }
+}
+
+library SaleKindInterface {
+
+    enum Side { Buy, Sell }
+
+    enum SaleKind { FixedPrice, DutchAuction }
+
+    function validateParameters(SaleKind saleKind, uint expirationTime)
+        pure
+        internal
+        returns (bool)
+    {
+        return (saleKind == SaleKind.FixedPrice || expirationTime > 0);
+    }
+
+    
+    function canSettleOrder(uint listingTime, uint expirationTime)
+        view
+        internal
+        returns (bool)
+    {
+        return (listingTime < block.timestamp) && (expirationTime == 0 || block.timestamp < expirationTime);
+    }
+
+    /**
+     * @dev Calculate the settlement price of an order
+     * @dev Precondition: parameters have passed validateParameters.
+     * @param side Order side
+     * @param saleKind Method of sale
+     * @param basePrice Order base price
+     * @param extra Order extra price data
+     * @param listingTime Order listing time
+     * @param expirationTime Order expiration time
+     */
+    function calculateFinalPrice(Side side, SaleKind saleKind, uint basePrice, uint extra, uint listingTime, uint expirationTime)
+        view
+        internal
+        returns (uint finalPrice)
+    {
+        if (saleKind == SaleKind.FixedPrice) {
+            return basePrice;
+        } else if (saleKind == SaleKind.DutchAuction) {
+            uint diff = SafeMath.div(SafeMath.mul(extra, SafeMath.sub(block.timestamp, listingTime)), SafeMath.sub(expirationTime, listingTime));
+            if (side == Side.Sell) {
+                /* Sell-side - start price: basePrice. End price: basePrice - extra. */
+                return SafeMath.sub(basePrice, diff);
+            } else {
+                /* Buy-side - start price: basePrice. End price: basePrice + extra. */
+                return SafeMath.add(basePrice, diff);
+            }
+        }
+    }
+
+}
+
