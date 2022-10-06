58c60213e09a64be20192e309c2d000fc414b891
JustAgent <baykov.ivan.03@gmail.com>
2022-10-06 23:19:55 +0300

    refactor

--- contracts/marketplaceNft/NftMarketplace.sol
+++ contracts/marketplaceNft/NftMarketplace.sol
@@ -6,14 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./iseaport.sol";
 
 contract Marketplace is Ownable, ReentrancyGuard {
-    
-    // address seaportAddress = 0x00000000006c3852cbEf3e08E8dF289169EdE581; //Goerli/Mainnet
-    // address connduit = 0x00000000F9490004C11Cef243f5400493c00Ad63;
-
-    // SeaportInterface seaport =  SeaportInterface(seaport);
+    using SafeMath for uint256;
 
     struct Order {
         address maker;
@@ -28,41 +23,52 @@ contract Marketplace is Ownable, ReentrancyGuard {
         uint expirationTime;
     }
     Order[] orders;
+    bytes32[] testSellOrders;
     mapping(bytes32 => bool) validOrders;
     mapping(bytes32 => bool) isSellOrder;
+    mapping(address => uint256) public nonces;
 
-    function transferNFTs(address _to, address, uint256[] tokenIds, address _contractAddress) returns(bool) {
+    function transferNFTs(
+        address _to, 
+        address, 
+        uint256[] memory tokenIds, 
+        address _contractAddress)
+        public 
+        returns(bool) 
+    {
         IERC721 nft = IERC721(_contractAddress);
         require(checkApproval(msg.sender, _contractAddress), "No allowance");
-        uint256 length = tokenIds.length();
+        uint256 length = tokenIds.length;
         for (uint i = 0; i < length; i++) {
-             _transferNFT(_to, tokenIs, _contractAddress);
+             _transferNFT(_to, tokenIds[i], nft);
         }
+        return true;
     }
 
-    function _transferNFT(address _to, address, uint256 tokenId, address _contractAddress ) private {
+    function _transferNFT(address _to, uint256 tokenId, IERC721 nft) private {
         nft.safeTransferFrom(msg.sender, _to, tokenId);
     }
 
     function checkApproval(address _user, address _contractAddress) private view returns(bool) {
-        IERC721 nft = IERC721(_contract);
+        IERC721 nft = IERC721(_contractAddress);
         return nft.isApprovedForAll(_user, address(this));
     }
 
     function createOrder(
         address maker,
-        address taker,
+        address taker, // Buy
         SaleKindInterface.Side side,
         SaleKindInterface.SaleKind saleKind,
         uint basePrice,
-        address paymentToken,
-        address target,
+        address paymentToken, 
+        address target, // Private selling if specified
         uint extra,
         uint listingTime,
         uint expirationTime
     )   private 
+        returns(Order memory)
     {
-        Order order = Order(
+        Order memory order = Order(
             maker,
             taker,
             side,
@@ -75,13 +81,21 @@ contract Marketplace is Ownable, ReentrancyGuard {
             expirationTime
         );
         require(validateOrderParameters(order), "Invalid order parameters");
-        
+        return order;
     }
 
-    function sell(SaleKindInterface.SaleKind saleKind, uint basePrice, address paymentToken, address target, uint extra, uint256 listingTime) {
+    function sell(
+        SaleKindInterface.SaleKind saleKind, 
+        uint basePrice, 
+        address paymentToken,
+        address target, 
+        uint extra, 
+        uint256 listingTime) 
+        public
+    {
         SaleKindInterface.Side side = SaleKindInterface.Side.Sell;
-        uint duration = mul(1 minutes, listingTime);
-        createOrder(
+        uint duration = listingTime.mul(1 minutes);
+        Order memory order = createOrder(
             msg.sender,
             address(0),
             side,
@@ -93,22 +107,39 @@ contract Marketplace is Ownable, ReentrancyGuard {
             duration,
             block.timestamp + duration
             );
+        
+        bytes32 orderHashed = hashOrder(order, nonces[msg.sender]);
+        incrementNonce(msg.sender);
+        testSellOrders.push(orderHashed);
     }
 
-    function validateOrderParameters(Order order) {
-        
+    function validateOrderParameters(Order memory order) private returns(bool) {
+         return true;
+    }
+
+
+    function hashOrder(Order memory order, uint nonce)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encode(order, nonce));
+    }
+
+    function incrementNonce(address user) private {
+        nonces[user] += 1;
     }
 
 }
 
 contract TokenRecipient{
     event ReceivedEther(address indexed sender, uint amount);
-    event ReceivedTokens(address indexed from, uint256 value, address indexed token, bytes extraData);
+    event ReceivedTokens(address indexed from, uint256 value, address indexed token);
 
-    function receiveApproval(address from, uint256 value, address token, bytes calldata extraData) public returns(bool) {
+    function receiveApproval(address from, uint256 value, address token) public returns(bool) {
         IERC20 t = IERC20(token);
         require(t.transferFrom(from, address(this), value));
-        emit ReceivedTokens(from, value, token, extraData);
+        emit ReceivedTokens(from, value, token);
         return true;
     }
 
@@ -146,16 +177,7 @@ library SaleKindInterface {
         return (listingTime < block.timestamp) && (expirationTime == 0 || block.timestamp < expirationTime);
     }
 
-    /**
-     * @dev Calculate the settlement price of an order
-     * @dev Precondition: parameters have passed validateParameters.
-     * @param side Order side
-     * @param saleKind Method of sale
-     * @param basePrice Order base price
-     * @param extra Order extra price data
-     * @param listingTime Order listing time
-     * @param expirationTime Order expiration time
-     */
+
     function calculateFinalPrice(Side side, SaleKind saleKind, uint basePrice, uint extra, uint listingTime, uint expirationTime)
         view
         internal
