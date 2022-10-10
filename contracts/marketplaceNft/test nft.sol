89e837c5bfdc2dd40fcce6ab3b613aa80e5619a1
JustAgent <baykov.ivan.03@gmail.com>
2022-10-10 20:02:40 +0300

    nfts for test

---
+++ contracts/marketplaceNft/test nft.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./NFT.sol";
+
+contract NFT_TEST1 is NFT {
+    constructor() NFT("TEST-1", "TEST-1") {
+  }
+}
+
+contract NFT_TEST2 is NFT {
+    constructor() NFT("TEST-2", "TEST-2") {
+  }
+}
+
+contract NFT_TEST3 is NFT {
+    constructor() NFT("TEST-3", "TEST-3") {
+  }
+}
